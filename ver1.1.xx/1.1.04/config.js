fdd4256b07a4251aaa600f81ee8b3ec76e8eab8b
Okakajp25 <115696838+Okakajp25@users.noreply.github.com>
2023-03-17 19:03:01 +0900

    Update config.js

--- ver1.1.xx/1.1.04/config.js
+++ ver1.1.xx/1.1.04/config.js
@@ -2,7 +2,7 @@ export default
     {
         icon: {
             hub: 'textures/items/bed_white',
-            mining: 'textures/items/bed_white',
+            mining: 'textures/items/wooden_pickaxe',
             shop: 'textures/items/emerald',
             pvp: 'textures/items/diamond_sword',
         },
