204e1cb3b2bc83b74cb6645b603afc06b66da954
Okakajp25 <115696838+Okakajp25@users.noreply.github.com>
2023-03-17 14:59:38 +0900

    Create

---
+++ ver1.1.xx/1.1.04/config.js
@@ -0,0 +1,12 @@
+export default
+    {
+        icon: {
+            hub: 'textures/items/bed_white',
+            mining: 'textures/items/bed_white',
+            shop: 'textures/items/emerald',
+            pvp: 'textures/items/diamond_sword',
+        },
+        command: {
+            prefix: '.'
+        },
+    }
