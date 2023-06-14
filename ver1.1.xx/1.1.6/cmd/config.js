ae615b85f156a07a8e1462526e18149d2af95651
おかーかじぇーぴー <115696838+Okakajp25@users.noreply.github.com>
2023-06-14 15:13:41 +0900

    create config.js

---
+++ ver1.1.xx/1.1.6/cmd/config.js
@@ -0,0 +1,23 @@
+export default
+    {
+        icon: {
+            hub: 'textures/items/bed_white',
+            mining: 'textures/items/wood_pickaxe',
+            shop: 'textures/items/emerald',
+            pvp: 'textures/items/diamond_sword',
+            coin: 'textures/items/icon_minecoin_9x9',
+        },
+        command: {
+            prefix: '.',
+        },
+        addon: {
+            githuburl: 'https://github.com/umbrellasaikutuserver/use-addon',
+            license: 'Apache License 2.0',
+            ver: {
+                scriptver: '1.1.5',
+                addonver: '1.0.5',
+            },
+        },
+        code: '2525',
+        codegift: '150'
+    }
